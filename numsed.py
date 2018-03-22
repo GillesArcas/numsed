7a563028a9f38a9e6f37e5830195facc35f9336f
Gilles Arcas <gilles.arcas@gmail.com>
2018-03-22 22:13:00 +0100

    Cleaning and refactoring. Better control of arguments.

--- numsed.py
+++ numsed.py
@@ -17,41 +17,29 @@ import snippet_test
 
 VERSION = '0.01'
 
-
-# AST
-# https://greentreesnakes.readthedocs.io/en/latest/
-
-# DST
-# https://docs.python.org/2/library/dis.html
-# http://unpyc.sourceforge.net/Opcodes.html
-# http://www.goldsborough.me/python/low-level/2016/10/04/00-31-30-disassembling_python_bytecode/
-# http://stackoverflow.com/questions/31989893/how-to-fully-disassemble-python-source
-# http://www.aosabook.org/en/500L/a-python-interpreter-written-in-python.html
-# https://github.com/python/cpython/blob/2bdba08bd0eb6f1b2a20d14558a4ea2009b46438/Python/ceval.c
-
-# http://faster-cpython-zh.readthedocs.io/en/latest/registervm.html
-
-
-def do_helphtml():
-    if os.path.isfile('README.md'):
-        helpfile = 'README.md'
-    else:
-        helpfile = r'https://github.com/GillesArcas/numsed/blob/master/README.md'
-
-    webbrowser.open(helpfile, new=2)
-
-
 USAGE = '''
-numsed.py -h | -H | -v
-numsed.py <format> <transformation> <action> python-script
-'''
+%s
+Version          %s
+Usage            numsed.py -h | -H
+                 numsed.py <action> <format> <transformation> python-script
+''' % (__doc__, VERSION)
+
 
 def parse_command_line(argstring=None):
-    parser = argparse.ArgumentParser(usage=USAGE, add_help=False)
+    parser = argparse.ArgumentParser(description=USAGE, usage=argparse.SUPPRESS, add_help=False, formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('-h', help='show this help message', action='store_true', dest='do_help')
-    parser.add_argument('-H', help='open html help page', action='store_true', dest='do_helphtml')
-    parser.add_argument("-v", help="version", action="store_true", dest="version")
+    agroup = parser.add_argument_group('Information')
+    xgroup = agroup.add_mutually_exclusive_group()
+    xgroup.add_argument('-h', help='show this help message', action='store_true', dest='help')
+    xgroup.add_argument('-H', help='open full help page', action='store_true', dest='fullhelp')
+
+    agroup = parser.add_argument_group('Actions')
+    xgroup = agroup.add_mutually_exclusive_group()
+    xgroup.add_argument("--run", help="run generated script (default)", action="store_true")
+    xgroup.add_argument("--trace", help="trace generated script", action="store_true")
+    xgroup.add_argument("--coverage", help="run intermediate opcode and display opcode coverage (--opcode only)", action="store_true")
+    xgroup.add_argument("--test", help="run conversion and compare with original python script", action="store_true")
+    xgroup.add_argument("--all", help="complete test", action="store_true")
 
     agroup = parser.add_argument_group('Formats')
     xgroup = agroup.add_mutually_exclusive_group()
@@ -59,21 +47,13 @@ def parse_command_line(argstring=None):
     xgroup.add_argument("--script", help="generate python script", action="store_true")
     xgroup.add_argument("--disassembly", help="generate disassembly", action="store_true")
     xgroup.add_argument("--opcode", help="generate numsed intermediate opcode", action="store_true")
-    xgroup.add_argument("--sed", help="generate sed script", action="store_true")
+    xgroup.add_argument("--sed", help="generate sed script (default)", action="store_true")
 
     agroup = parser.add_argument_group('Transformations')
     xgroup = agroup.add_mutually_exclusive_group()
     xgroup.add_argument("--literal", help="no program transformation", action="store_true")
     xgroup.add_argument("--unsigned", help="replace division, modulo and power by functions", action="store_true")
-    xgroup.add_argument("--signed", help="replace all operators by functions", action="store_true")
-
-    agroup = parser.add_argument_group('Actions')
-    xgroup = agroup.add_mutually_exclusive_group()
-    xgroup.add_argument("--run", help="run generated script", action="store_true")
-    xgroup.add_argument("--trace", help="trace generated script", action="store_true")
-    xgroup.add_argument("--coverage", help="run intermediate opcode and display opcode coverage (--opcode only)", action="store_true")
-    xgroup.add_argument("--test", help="run conversion and compare with original python script", action="store_true")
-    xgroup.add_argument("--all", help="complete test", action="store_true")
+    xgroup.add_argument("--signed", help="replace all operators by functions (default)", action="store_true")
 
     parser.add_argument("source", nargs='?', help=argparse.SUPPRESS)
 
@@ -82,10 +62,44 @@ def parse_command_line(argstring=None):
     else:
         args = parser.parse_args(argstring.split())
 
+    information = (args.help, args.fullhelp)
+
+    actions = (args.trace, args.run, args.coverage, args.test, args.all)
+    if not any(actions):
+        args.run = True
+
+    formats = (args.ast, args.script, args.disassembly, args.opcode, args.sed)
+    if not any(formats):
+        args.sed = True
+
+    transformations = (args.literal, args.unsigned, args.signed)
+    if not any(transformations):
+        args.signed = True
+
+    # no action specified nor filename
+    if not any(information) and not any(actions) and args.source is None:
+        parser.print_help()
+        parser.exit(1)
+
+    # some action but no filename
+    if not any(information) and args.source is None:
+        print('numsed.py: error: filename required')
+        parser.exit(1)
+
     if args.coverage and not args.opcode:
-        print('numsed error: --coverage requires --opcode')
-        exit(1)
+        print('numsed.py: error: argument --coverage requires argument --opcode')
+        parser.exit(1)
+
     return parser, args
+
+
+def do_fullhelp():
+    if os.path.isfile('README.md'):
+        helpfile = 'README.md'
+    else:
+        helpfile = r'https://github.com/GillesArcas/numsed/blob/master/README.md'
+
+    webbrowser.open(helpfile, new=2)
 
 
 def transformation(args):
@@ -94,10 +108,6 @@ def transformation(args):
     elif args.unsigned:
         return transformer.UNSIGNED
     elif args.signed:
-        return transformer.SIGNED
-    else:
-        # default to --signed
-        args.signed = True
         return transformer.SIGNED
 
 
@@ -112,13 +122,9 @@ def numsed_maker(args):
         return opcoder.OpcodeConversion
     elif args.sed:
         return sedcode.SedConversion
-    else:
-        # default to --sed
-        args.sed = True
-        return sedcode.SedConversion
 
 
-def proceed_test(args, source):
+def process_test(args, source):
     maker = numsed_maker(args)
     target = maker(source, transformation(args))
     if args.run:
@@ -129,10 +135,7 @@ def proceed_test(args, source):
         x = target.test()
     elif args.trace:
         x = target.trace()
-    else:
-        # default to --run
-        args.run = True
-        x = target.run()
+
     if args.run and args.sed:
         # already printed
         pass
@@ -147,17 +150,18 @@ def process_suite(args):
         print(test[0].rstrip())
         with open('tmp.py', 'w') as f:
             f.writelines(test)
-        r = proceed_test(args, 'tmp.py')
+        r = process_test(args, 'tmp.py')
         status = status and (not args.test or r)
         if not status:
             break
-    print('ALL TESTS OK' if status else 'ONE TEST FAILURE')
+    if args.test:
+        print('ALL TESTS OK' if status else 'ONE TEST FAILURE')
     return status
 
 
 def process_all(args):
-    # use --trace when -test is not relevant. This may catch some errors
-    # and complete coverage.
+    # use --trace when -test is not relevant. This completes coverage and may
+    # catch some errors
     test_args = ('--ast    --literal  --test ',
                     '--ast    --unsigned --trace',
                     '--ast    --signed   --test ',
@@ -181,15 +185,15 @@ def process_all(args):
 def numsed(argstring=None):
     parser, args = parse_command_line(argstring)
 
-    if args.version:
+    if False: # args.version:
         print(__doc__)
         print(VERSION)
 
-    elif args.do_help:
+    elif args.help:
         parser.print_help()
 
-    elif args.do_helphtml:
-        do_helphtml()
+    elif args.fullhelp:
+        do_fullhelp()
 
     elif args.all:
         process_all(args)
@@ -198,7 +202,7 @@ def numsed(argstring=None):
         if args.source.endswith('.suite.py'):
             result = process_suite(args)
         else:
-            result = proceed_test(args, args.source)
+            result = process_test(args, args.source)
         if args.coverage:
             opcoder.display_coverage()
         return result
